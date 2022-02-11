6c7fefeffdec92353ade4fcc05845bd1f2efb05f
top9xlee <72817031+top9xlee@users.noreply.github.com>
2022-02-11 08:39:46 +0700

    first commit

---
+++ New folder/Step2/src/main/java/com/Website/Step2/controller/PostController.java
@@ -0,0 +1,55 @@
+package com.Website.Step2.controller;
+import com.Website.Step2.dto.PostRequest;
+import com.Website.Step2.dto.PostResponse;
+import com.Website.Step2.service.PostService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.status;
+
+@RestController
+@RequestMapping("/api/posts/")
+@AllArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<Void> createPost(@RequestBody PostRequest postRequest) {
+        postService.save(postRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        return status(HttpStatus.OK).body(postService.getAllPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponse> getPost(@PathVariable Long id) {
+        return status(HttpStatus.OK).body(postService.getPost(id));
+    }
+
+    @GetMapping("by-sub/{id}")
+    public ResponseEntity<List<PostResponse>> getPostsBySub(@PathVariable Long id) {
+        return status(HttpStatus.OK).body(postService.getPostsBySub(id));
+    }
+
+    @GetMapping("by-user/{name}")
+    public ResponseEntity<List<PostResponse>> getPostsByUsername(@PathVariable String name) {
+        return status(HttpStatus.OK).body(postService.getPostsByUsername(name));
+    }
+    @GetMapping("by-des/{name}")
+    public ResponseEntity<List<PostResponse>> getPostByDescription(@PathVariable String name){
+        return status (HttpStatus.OK).body(postService.getPostByDescription(name));
+    }
+    @GetMapping("by-post/{name}")
+    public ResponseEntity<List<PostResponse>> getPostByPost(@PathVariable String name){
+        return status (HttpStatus.OK).body(postService.getPostByPost(name));
+    }
+
+}
---
+++ New folder/Step2/src/main/java/com/Website/Step2/service/CommentService.java
@@ -0,0 +1,75 @@
+package com.Website.Step2.service;
+
+
+import com.Website.Step2.dto.CommentsDto;
+import com.Website.Step2.exception.PostNotFoundException;
+import com.Website.Step2.mapper.CommentMapper;
+import com.Website.Step2.model.Comment;
+import com.Website.Step2.model.NotificationEmail;
+import com.Website.Step2.model.Post;
+import com.Website.Step2.model.User;
+import com.Website.Step2.repository.CommentRepository;
+import com.Website.Step2.repository.PostRepository;
+import com.Website.Step2.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+@Transactional
+public class CommentService {
+
+    private static final String POST_URL = "";
+    private final CommentMapper commentMapper;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    public void save(CommentsDto commentsDto) {
+        Post post = postRepository.findById(commentsDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
+        Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(authService.getCurrentUser() + " posted a comment on your post." + POST_URL);
+        sendCommentNotification(message, post.getUser());
+    }
+    public void delete(Long id){
+
+        commentRepository.deleteById(id);
+    }
+
+    private void sendCommentNotification(String message, User user) {
+        mailService.sendMail(new NotificationEmail(user.getUsername() + " Commented on your post", user.getEmail(), message));
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(postId.toString()));
+        return commentRepository.findByPost(post)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .collect(toList());
+    }
+
+    public List<CommentsDto> getAllCommentsForUser(String userName) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+        return commentRepository.findAllByUser(user)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .collect(toList());
+    }
+
+
+}
